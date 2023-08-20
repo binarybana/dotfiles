b1181fed9a6892a60102e230b58133b7c57f0d8b
Jason Knight <Jason@JasonKnight.us>
2023-08-20 16:11:11 -0700

    Make uparrow the same

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -73,7 +73,6 @@ if status is-interactive
     complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
   end
 
-  atuin init fish | source
-  # If I want to disable up arrow ^^^: --disable-up-arrow
+  atuin init fish --disable-up-arrow | source
 end
 
