4a4a55614d09438f147c625c648c45a245a22d48
Jason Knight <Jason@JasonKnight.us>
2023-08-20 16:52:09 -0700

    Add devup function

---
+++ home/.config/fish/functions/devup.fish
@@ -0,0 +1,29 @@
+function devup
+  get_ip
+
+  if test $ip_address = null
+    echo "Instance not started, starting instance..."
+    aws ec2 start-instances --instance-ids i-07884ad40ea9456ce --profile sandbox
+    sleep 10
+    get_ip
+  end
+
+  mkdir -p ~/.ssh/config.d
+  echo -e "Host aws\n  HostName $ip_address\n  IdentityFile ~/.ssh/jknight.pem\n  User ubuntu" > ~/.ssh/config.d/aws
+
+  echo "SSHing into instance"
+  ssh aws
+end
+
+function devdown
+  aws ec2 stop-instances --instance-ids i-07884ad40ea9456ce --profile sandbox
+end
+
+function devlogin
+  aws sso login --sso-session octosession
+end
+
+function get_ip
+  set -g ip_address (aws ec2 describe-instances --instance-ids i-07884ad40ea9456ce --output json --profile sandbox |
+          jq -r '.Reservations[].Instances[].PublicIpAddress')
+end
