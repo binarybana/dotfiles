2c2f1c79e6b90bf864d6e9d77489b18359257ba8
Jason Knight <392846+binarybana@users.noreply.github.com>
2025-02-28 17:03:13 -0800

    MLIR and Triton LSP server config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -489,6 +489,13 @@ require("lazy").setup({
 					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+					-- From Matthew
+					map("<leader>cs", vim.lsp.buf.signature_help, "Show [c]ode [s]ignature help")
+					map("<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
+					map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
+					map("<leader>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, "List workspace folders")
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
@@ -547,84 +554,33 @@ require("lazy").setup({
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			-- Enable the following language servers
-			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-			--
-			--  Add any additional override configuration in the following tables. Available keys are:
-			--  - cmd (table): Override the default command used to start the server
-			--  - filetypes (table): Override the default list of associated filetypes for the server
-			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-			--  - settings (table): Override the default settings passed when initializing the server.
-			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-			--[=====[
-			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				-- ts_ls = {},
-				--
+		local lspconfig = require "lspconfig"		
+		require("lspconfig.configs").mlir = {
+		  default_config = {
+		    cmd = { 'triton-lsp' },
+		    filetypes = { 'mlir' },
+		    root_dir = lspconfig.util.root_pattern('.git'),
+		  },
+		}
+		require("lspconfig.configs").tablegen = {
+		  default_config = {
+		    cmd = { 'tblgen-lsp-server', '--tablegen-compilation-database=tablegen_compile_commands.yml' },
+		    filetypes = { 'tablegen' },
+		    root_dir = lspconfig.util.root_pattern('.git'),
+		  },
+		}
+		require("lspconfig").mlir.setup({capabilities = capabilities})
+		require("lspconfig").tablegen.setup({capabilities = capabilities})
+		require("lspconfig").ruff.setup({})
+		require("lspconfig").pyright.setup({})
+		require("lspconfig").clangd.setup({})
+		require("lspconfig").rust_analyzer.setup({
+		  on_attach = function(client, bufnr)
+		      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+		  end
+		})
 
-				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
-						},
-					},
-				},
-			}
-
-			-- Ensure the servers and tools above are installed
-			--  To check the current status of installed tools and/or manually install
-			--  other tools, you can run
-			--    :Mason
-			--
-			--  You can press `g?` for help in this menu.
-			require("mason").setup()
-
-			-- You can add other tools here that you want Mason to install
-			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
-			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for ts_ls)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
-      --]=====]
-			require("lspconfig").ruff.setup({})
-			require("lspconfig").pyright.setup({})
-			require("lspconfig").clangd.setup({})
-			require("lspconfig").rust_analyzer.setup({
-			    on_attach = function(client, bufnr)
-			        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-			    end
-			})
-		end,
+	end,
 	},
 
 	{ -- Autoformat
@@ -656,7 +612,9 @@ require("lazy").setup({
 				end
 				return {
 					timeout_ms = 500,
-					lsp_format = lsp_format_opt,
+					lsp_format = "never",
+					-- JASON: SETTING TO NEVER FOR TRITON
+					-- lsp_format = lsp_format_opt,
 				}
 			end,
 			formatters_by_ft = {
@@ -922,16 +880,33 @@ require("lazy").setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
+
 if os.getenv("ON_NV") then
+	-- From https://github.com/neovim/neovim/discussions/28010#discussioncomment-9877494
+	vim.o.clipboard = "unnamedplus"
+
+	local function paste()
+	  return {
+	    vim.fn.split(vim.fn.getreg(""), "\n"),
+	    vim.fn.getregtype(""),
+	  }
+	end
+
 	vim.g.clipboard = {
-	  name = 'OSC 52',
+	  name = "OSC 52",
 	  copy = {
-	    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-	    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+	    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+	    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
 	  },
 	  paste = {
-	    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-	    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+	    ["+"] = paste,
+	    ["*"] = paste,
 	  },
 	}
 end
+
+-- Associate triton files with mlir
+vim.filetype.add({ extension = { ptx = 'asm' } })
+vim.filetype.add({ extension = { ttir = 'mlir' } })
+vim.filetype.add({ extension = { ttgir = 'mlir' } })
+vim.filetype.add({ extension = { mlir = 'mlir' } })
